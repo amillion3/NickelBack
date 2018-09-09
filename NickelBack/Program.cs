9da897575c2627ede9816f18e5ccad85299fb7b7
Andy Million <amillion3@gmail.com>
2018-09-09 14:51:08 -0500

    initial commit

--- NickelBack/Program.cs
+++ NickelBack/Program.cs
@@ -6,7 +6,14 @@ namespace NickelBack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            // Define a class called Song that has a string property Artist and a string property Name.Both properties should be required(invariant) in order to construct an instance of Song
+            //Define a List, named goodSongs, that will hold instances of Song.
+            //Define a HashSet, named allSongs, that contains 7 instances of Song.
+            //Make sure that some of the songs are from the band Nickelback. You can see a list of their greatest hits on Amazon.
+            //Once the set is populated with 7 instances, iterate over the set of songs, and check if the band name is "Nickelback".
+            //If the band is not Nickelback, then add it to the goodSongs list.
+            //Use another foreach loop to print out all the good songs.
         }
     }
 }
