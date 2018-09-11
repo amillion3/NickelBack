70efa6be02d265b298e2a3f35a35d6a11bbd14d0
Andy Million <amillion3@gmail.com>
2018-09-10 19:01:44 -0500

    loops complete

--- NickelBack/Program.cs
+++ NickelBack/Program.cs
@@ -17,38 +17,22 @@ namespace NickelBack
             allSongs.Add(new Song("Nickelback", "Feed The Machine"));
             allSongs.Add(new Song("Alice in Chains", "Would"));
 
-            //for (int i = 0; i < allSongs.Count; i++)
-            //{
-            //    object o = allSongs[i];
-            //}
-
-            //for (int i = 0; i < allSongs.Count; i++)
-            //{
-            //    Song value;
-            //    if (allSongs.TryGetValue(key "Nickelback", out value) {
-
-            //    }
-            //}
-
+            // Iterate over allSongs and save non-Nickelback to a new HashSet
             foreach (Song i in allSongs)
             {
-                Console.WriteLine(i.GetArtistName());
-                
+                if (i.GetArtistName != "Nickelback")
+                {
+                    goodSongs.Add(new Song(i.GetArtistName, i.GetSongName));
+                }
+            }
+
+            // Print out non-Nickelback songs
+            foreach (Song j in goodSongs)
+            {
+                Console.WriteLine($"{j.GetArtistName} - {j.GetSongName}");
             }
 
             Console.ReadLine();
-
-            // Define a class called Song that has a string property Artist and a string property Name.
-            // Both properties should be required(invariant) in order to construct an instance of Song
-            //Define a List, named goodSongs, that will hold instances of Song.
-            //Define a HashSet, named allSongs, that contains 7 instances of Song.
-            //Make sure that some of the songs are from the band Nickelback. You can see a list of 
-            // their greatest hits on Amazon.
-
-            //Once the set is populated with 7 instances, iterate over the set of songs, 
-            // and check if the band name is "Nickelback".
-            //If the band is not Nickelback, then add it to the goodSongs list.
-            //Use another foreach loop to print out all the good songs.
         }
     }
 }
--- NickelBack/Song.cs
+++ NickelBack/Song.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NickelBack
+﻿namespace NickelBack
 {
     class Song
     {
         protected readonly string _artist;
         protected readonly string _name;
+
+        public string GetArtistName {
+            get { return _artist; }
+        }
+
+        public string GetSongName
+        {
+            get { return _name; }
+        }
 
         public Song(string artist, string name)
         {
@@ -15,8 +20,5 @@ namespace NickelBack
             _name = name;
         }
 
-        public string GetArtistName() {
-            return _artist;
-        }
     }
 }
